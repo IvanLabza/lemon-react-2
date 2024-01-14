28d0655131ce918652b46281c9117e95269300eb
IvanLabza <vanalabza77@gmail.com>
2024-01-14 23:25:30 +0200

    main

--- src/componet/Header/Header.jsx
+++ src/componet/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import HeaderList from "./HeaderList/HeaderList";
 import logo from "../../assets/logo.svg";
-import open from "../../assets/symbol-defs.svg#paragraph-left";
-import close from "../../assets/symbol-defs.svg#cross";
+import open from "../../assets/main-menu.png";
+import close from "../../assets/multiply.png";
 
 function Header() {
+  const [isClassAdded, setIsClassAdded] = useState(false);
+
+  const toggleClass = () => {
+    setIsClassAdded(!isClassAdded);
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -13,7 +19,13 @@ function Header() {
             <img width="34" height="34" src={logo} alt="" />
             Zone.
           </a>
-          <div className="wrap">
+          <button onClick={toggleClass} className="header__open ">
+            Menu <img src={open} alt="" />
+          </button>
+          <div className={isClassAdded ? "wrap is-open" : "wrap"}>
+            <button onClick={toggleClass} className="header__open close">
+              Menu <img src={close} alt="" />
+            </button>
             <HeaderList></HeaderList>
             <a href="" className="header__link">
               Contact Us
@@ -24,6 +36,5 @@ function Header() {
     </header>
   );
 }
-
 
 export default Header;
